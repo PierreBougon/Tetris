2764e6fc51d36ce841c36283520a5db0167e74c3
clemen_j <cedric.clemenceau@epitech.eu>
2016-02-23 23:20:57 +0100

    $1

--- src/main/tetris.c
+++ src/main/tetris.c
@@ -5,7 +5,7 @@
 ** Login   <bougon_p@epitech.net>
 **
 ** Started on  Tue Feb 23 17:12:05 2016 bougon_p
-** Last update Tue Feb 23 22:03:36 2016 Clémenceau Cedric
+** Last update Tue Feb 23 22:42:36 2016 Clémenceau Cedric
 */
 
 #include "tetris.h"
@@ -13,27 +13,33 @@
 
 void	write_tetris()
 {
-  printw("						PLEURE PAS <3\n");
-  printw("*** *** *** *** * ***\n");
-  printw(" *  *    *  * *   *  \n");
-  printw(" *  ***  *   ** * ***\n");
-  printw(" *  *    *  * * *   *\n");
-  printw(" *  ***  *  * * * ***\n");
-  printw("\n\n\n");
+  attron(COLOR_PAIR(1));
+  printw("*** *** *** *** * ***  ------------   /Next--\\\n");
+  printw(" *  *    *  * *   *    |          |   |      |\n");
+  printw(" *  ***  *   ** * ***  |          |   |      |\n");
+  printw(" *  *    *  * * *   *  |          |   \\------/\n");
+  printw(" *  ***  *  * * * ***  |          | \n");
+  printw("                       |          | \n");
+  printw("                       |          | \n");
+  printw("                       |          | \n");
 }
 
 void	print_score()
 {
-  printw(" /------------------\\\n");
-  printw(" |                  |\n");
-  printw(" | High Score       |\n");
-  printw(" | Score            |\n");
-  printw(" |                  |\n");
-  printw(" | Lines            |\n");
-  printw(" | Level            |\n");
-  printw(" |                  |\n");
-  printw(" | Timer            |\n");
-  printw(" \\------------------/\n");
+  printw(" /------------------\\  |          | \n");
+  printw(" |                  |  |          | \n");
+  printw(" | High Score       |  |          | \n");
+  printw(" | Score            |  |          | \n");
+  printw(" |                  |  |          | \n");
+  printw(" | Lines            |  |          | \n");
+  printw(" | Level            |  |          | \n");
+  printw(" |                  |  |          | \n");
+  printw(" | Timer            |  |          | \n");
+  printw(" \\------------------/  |          | \n");
+  printw("                       |          | \n");
+  printw("                       |          | \n");
+  printw("                       |          | \n");
+  printw("EpiTetris(c) - 2015    ------------ \n");
 }
 
 void	aff_layout()
@@ -51,6 +57,8 @@ int	main(int ac, char **av, char **env)
       return (-1);
     }
   initscr();
+  start_color();
+  init_pair(1, COLOR_BLUE, COLOR_BLACK);
   aff_layout();
   refresh();
   getch();
